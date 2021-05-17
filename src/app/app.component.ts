fbe41faec49a1afbfc879da4005ad45824e34843
LaurainClem <c.laurain@vocaza.lan>
2021-05-17 11:40:54 +0200

    ✨ hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+  menuItems = ['home', 'about me', 'projects', 'contact'];
+
+  constructor(private readonly translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.setDefaultLang('en');
+  }
 }
