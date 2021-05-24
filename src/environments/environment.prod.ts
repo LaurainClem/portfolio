db36b53bf54e86ff1f53b0b2deff7037773f1507
Laurain Clement <c.laurain@vocaza.com>
2021-05-24 16:58:07 +0200

    🚀 (SSL): Change envioronement settings to apply https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	graphQLBaseUrl: 'http://167.172.177.134:1337/graphql?query=',
-	baseUrl: 'http://167.172.177.134:1337',
+	graphQLBaseUrl: 'https://laurainc.fr/graphql?query=',
+	baseUrl: 'https://laurainc.fr',
 };
