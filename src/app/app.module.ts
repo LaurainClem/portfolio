93b8e439a00316dc758c1cd156b5deb964855ebb
Laurain Clem <laurain.clement.contact@gmail.com>
2021-05-26 22:06:43 +0200

    🚀 (Barebone): Barebone version 2
    Supress components to restart from clear architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,9 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { LangsComponent } from './langs/langs.component';
 import { GraphqlInterceptor } from './graphql.interceptor';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
-import { LiveComponent } from './live/live.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
 	return new TranslateHttpLoader(http);
@@ -49,7 +44,7 @@ export function markedOptionsFactory(): MarkedOptions {
 }
 
 @NgModule({
-	declarations: [AppComponent, HeroComponent, AboutComponent, ProjectsComponent, LangsComponent, LiveComponent],
+	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
